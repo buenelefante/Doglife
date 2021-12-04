5040877dcdd1083173898f1173a74fdeb5b4aed1
NWpiter <northboss@gmail.com>
2021-12-04 17:33:43 +0300

    add phpmailer

--- mail.php
+++ mail.php
@@ -1,11 +1,45 @@
 <?php
-    ini_set( 'display_errors', 1 );
-    error_reporting( E_ALL );
-    $from = "vovaslon@mail.ru";
-    $to = "volium1987@mail.ru";
-    $subject = "Вопрос с сайта Doglife";
-    $message = "проверка связи";
-    $headers = "From:" . $from;
-    mail($to,$subject,$message, $headers);
-    echo "Сообщение отправлено";
-?>
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+$message = $_POST['message'];
+
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.mail.ru';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'vovaslon@mail.ru';                     //SMTP username
+    $mail->Password   = 'pupsik';                               //SMTP password
+    $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('vovaslon@mail.ru', 'Customer');
+    $mail->addAddress('northboss@gmail.com', 'Joe User');     //Add a recipient
+   
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Заявка с сайта Doglife';
+    $mail->Body    = .$name . 'Есть вопрос, мой телефон' . $phone . 'почта: ' . $email ' . $message;
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
